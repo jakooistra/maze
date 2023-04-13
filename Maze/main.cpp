a0461440d1c6d8a0d9ba3b0e4f6dcf402d0e474b
John Kooistra <jakooistra@gmail.com>
2023-04-13 19:19:02 -0400

    Initial Commit

---
+++ Maze/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Maze
+//
+//  Created by John Kooistra on 2023-04-13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
