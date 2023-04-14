433d9740e9ed7d8d4b13f831ed196de4a3a6100b
John Kooistra <jakooistra@gmail.com>
2023-04-14 01:19:36 -0400

    Add analysis object

---
+++ Maze/analysis.cpp
@@ -0,0 +1,26 @@
+//
+//  analysis.cpp
+//  Maze
+//
+//  Created by John Kooistra on 2023-04-13.
+//
+
+#include <iostream>
+
+#include "analysis.h"
+
+void Analysis::print() {
+    std::cout << "Analysis:" << std::endl;
+    if (isSolvable()) {
+        std::cout << "  Shortest path is " << shortestPath.size() << " long." << std::endl;
+        std::cout << "  Solution is " << (singularPath ? "singular." : "not singular.") << std::endl;
+    } else {
+        std::cout << "  Maze is not solvable." << std::endl;
+    }
+    
+    std::cout << "  Dead ends: " << branches.size() << std::endl;
+    
+    int totalCells = reachableCells + unreachableCells;
+    int percentReachable = (100 * reachableCells) / totalCells;
+    std::cout << "  Reachability: " << percentReachable << "% of cells can be reached." << std::endl;
+}
---
+++ Maze/analysis.h
@@ -0,0 +1,40 @@
+//
+//  analysis.h
+//  Maze
+//
+//  Created by John Kooistra on 2023-04-13.
+//
+
+#ifndef analysis_h
+#define analysis_h
+
+#include <vector>
+
+#include "xy.h"
+
+struct BranchEndpoint {
+    XY position;
+    int length;
+};
+
+struct Analysis {
+    // Shortest solution, from start to finish inclusive.
+    std::vector<XY> shortestPath;
+    
+    // True if there is only one way to solve the maze.
+    bool singularPath;
+    
+    // The length and location of all branch dead ends.
+    std::vector<BranchEndpoint> branches;
+    
+    // How many cells were reachable/unreachable in the analyzed maze.
+    int reachableCells;
+    int unreachableCells;
+    
+    // Returns true if there is a solution to the analyzed maze.
+    bool isSolvable() const { return !shortestPath.empty(); }
+    
+    void print();
+};
+
+#endif /* analysis_h */
