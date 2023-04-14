b4374e1484afce5f98533739ff2eb89155a9c6c8
John Kooistra <jakooistra@gmail.com>
2023-04-13 23:46:29 -0400

    Improve start/finish visualization

--- Maze/mazetoimage.cpp
+++ Maze/mazetoimage.cpp
@@ -23,25 +23,28 @@ static void clearLeftWall(Image *image, XY pos, int wallWidth, int cellWidth, un
     paintRect(image, pos.x * stride, wallWidth + pos.y * stride, wallWidth, cellWidth, value);
 }
 
-static void indicateStartEnd(Image *image, Maze const *maze, XY pos, bool isStart, int wallWidth, int cellWidth) {
-    unsigned char value = 128;
+static void indicateStartEnd(Image *image, Maze const *maze, XY pos, int wallWidth, int cellWidth, unsigned char value = 128) {
+    int stride = cellWidth + wallWidth;
+    int inset = std::max(0, std::min(2, (cellWidth - 1) / 2));
+    paintRect(image,
+              wallWidth + pos.x * stride + inset,
+              wallWidth + pos.y * stride + inset,
+              cellWidth - inset * 2,
+              cellWidth - inset * 2,
+              value);
+
     if (pos.x == 0) {
         clearLeftWall(image, pos, wallWidth, cellWidth, value);
+        paintRect(image, wallWidth + pos.x * stride, stride / 2 + pos.y * stride, inset, wallWidth, value);
     } else if (pos.x == maze->getWidth() - 1) {
         clearLeftWall(image, XY(pos.x + 1, pos.y), wallWidth, cellWidth, value);
+        paintRect(image, (pos.x + 1) * stride - inset, stride / 2 + pos.y * stride, inset, wallWidth, value);
     } else if (pos.y == 0) {
         clearTopWall(image, pos, wallWidth, cellWidth, value);
+        paintRect(image, stride / 2 + pos.x * stride, wallWidth + pos.y * stride, wallWidth, inset, value);
     } else if (pos.y == maze->getHeight() - 1) {
         clearTopWall(image, XY(pos.x, pos.y + 1), wallWidth, cellWidth, value);
-    } else {
-        int stride = cellWidth + wallWidth;
-        int inset = std::max(0, std::min(2, (cellWidth - 1) / 2));
-        paintRect(image,
-                  wallWidth + pos.x * stride + inset,
-                  wallWidth + pos.y * stride + inset,
-                  cellWidth - inset * 2,
-                  cellWidth - inset * 2,
-                  value);
+        paintRect(image, stride / 2 + pos.x * stride, (pos.y + 1) * stride - inset, wallWidth, inset, value);
     }
 }
 
@@ -69,17 +72,13 @@ std::unique_ptr<Image> convertToImage(Maze const *maze, int wallWidth, int cellW
                 clearLeftWall(image.get(), xy, wallWidth, cellWidth);
             }
             
-            // Start and end definition
-            if (xy == maze->getStart()) {
-                indicateStartEnd(image.get(), maze, xy, true, wallWidth, cellWidth);
-            }
-            if (xy == maze->getFinish()) {
-                indicateStartEnd(image.get(), maze, xy, false, wallWidth, cellWidth);
-            }
-            
             // TODO: pathing? Or do that as an extra pass?
         }
     }
+    
+    // Start and end definition
+    indicateStartEnd(image.get(), maze, maze->getStart(), wallWidth, cellWidth);
+    indicateStartEnd(image.get(), maze, maze->getFinish(), wallWidth, cellWidth);
     
     return image;
 }
