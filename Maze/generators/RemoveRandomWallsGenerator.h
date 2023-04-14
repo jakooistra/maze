b8710499c4a63825514770eeb127b85ba74fa0a4
John Kooistra <jakooistra@gmail.com>
2023-04-14 18:02:21 -0400

    Add stats to generator header files.

--- Maze/generators/RemoveRandomWallsGenerator.h
+++ Maze/generators/RemoveRandomWallsGenerator.h
@@ -11,6 +11,26 @@
 #include "MazeGenerator.h"
 
 // Generates a maze by randomly selecting walls to remove.
+//
+// Properties: highly variable, not guaranteed to be solvable, lots of
+// unreachable areas, very quick to generate.
+//
+// Stats:
+//  250 mazes analyzed, 16x16
+//  36% solvable
+//  0% of solvable mazes have singular solutions
+//  15 average path length
+//  6% of solvable mazes are trivial
+//  63% of cells are unreachable
+//  15 average degenerate corners per maze
+//  16 dead ends per solvable maze on average
+//    33-64: XXX
+//    17-32: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//     9-16: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//      5-8: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//      3-4: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//        2: XXXXXXXXXXXXXXXXXXXX
+//        1: XXXXXXXXXXXXXXXXXXXXX
 class RemoveRandomWallsGenerator : public MazeGenerator {
 public:
     RemoveRandomWallsGenerator() : MazeGenerator(MazeType::RemoveRandomWalls) {}
--- Maze/generators/VerticalPathBreaksGenerator.h
+++ Maze/generators/VerticalPathBreaksGenerator.h
@@ -11,6 +11,20 @@
 #include "MazeGenerator.h"
 
 // Generates a maze by creating vertical hallways, and randomly removing walls between them.
+//
+// Properties: kind of boring, guaranteed to be solvable and singular, long
+// paths (averaging 40% of all available cells), bounded but obvious dead ends.
+//
+// Stats:
+//  250 mazes analyzed, 16x16
+//  100% of solvable mazes have singular solutions
+//  101 average path length
+//  28 dead ends per solvable maze on average
+//    9-16: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//     5-8: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//     3-4: XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//       2: XXXXXXXXXXXXXXXXXXXXXXXX
+//       1: XXXXXXXXXXXXXXXXXXXXXXXXXXX
 class VerticalPathBreaksGenerator : public MazeGenerator {
 public:
     VerticalPathBreaksGenerator() : MazeGenerator(MazeType::VerticalPathBreaks) {}
