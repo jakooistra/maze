31a3cc7767a772cb0d0fdc82d3c4eeb822fe6537
John Kooistra <jakooistra@gmail.com>
2023-04-13 22:04:09 -0400

    Update project file to run with a test harness

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -7,8 +7,30 @@
 
 #include <iostream>
 
+#include "lodepng.h"
+#include "maze.h"
+#include "mazetoimage.h"
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Maze maze(3, 3);
+    maze[{0, -1}].topWall = false;
+    maze[{0, 0}].topWall = false;
+    maze[{0, 1}].topWall = false;
+    maze[{2, 2}].leftWall = false;
+    maze[{1, 1}].leftWall = false;
+    
+    auto image = convertToImage(&maze, 4, 20);
+    
+    std::vector<unsigned char> rgba;
+    image->encodeRGBA(rgba);
+    
+    auto error = lodepng::encode("maze.png", &rgba[0], image->width, image->height);
+    
+    if (error != 0) {
+        std::cout << "Image written with error code: " << lodepng_error_text(error) << std::endl;
+    } else {
+        std::cout << "Image written sccessfully." << std::endl;
+    }
+    
     return 0;
 }
